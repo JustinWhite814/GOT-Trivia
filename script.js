c10fc50e272489f92623cc011e5d998990bd1fbc
Juswhite814 <juswhite814@gmail.com>
2021-04-10 16:10:44 -0400

    new edits

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ const backBtn = document.querySelector('.home')
 const rules = document.querySelector('.rules')
 const modal = document.querySelector('.modal')
 const noModal = document.querySelector('.noModal')
-const close = document.querySelector('#close')
-
+const close = document.querySelectorAll('#close')
+const nuclear = document.querySelector('.nuclear')
 const section = document.querySelector('section')
 
 let questions = [
@@ -66,7 +66,7 @@ let questions = [
     let questions2 = [
     
         {
-            question: "OF which house was John Snow truly born",
+            question: "Of which house was John Snow born?",
             a: 'Targaryen',
             b: 'Sand',
             c: 'Stark',
@@ -83,16 +83,16 @@ let questions = [
             correctAnswer: 'c'
         },
         {
-            question: "Which house has the motto: 'A _____ always repays their debts ",
-                a: 'House Lannister',
-                b: 'House Targaryen',
-                c: 'House Stark'
+            question: "A _____ always repays their debts?",
+                a: 'Lannister',
+                b: 'Targaryen',
+                c: 'Stark'
             ,
             correctAnswer: 'a'
 
         },
         {
-            question: 'Which member of which House trained on the Island of Bravos',
+            question: 'Which member of which House trained on the Island of Bravos?',
                 a: 'Aria, of House Stark',
                 b: 'Sam, of House Tully', 
                 c: 'Lyanna, of House Mormont',
@@ -100,7 +100,7 @@ let questions = [
           correctAnswer: 'a'      
         },
         {
-            question: 'Which Member/s of House Lannister killed themselves',
+            question: 'Which Member/s of House Lannister killed themselves?',
                 a: 'Joffrey, Tywin',
                 b: 'Tommen', 
                 c: 'Jaimie',
@@ -138,7 +138,7 @@ let questions = [
 
         },
         {
-            question: 'What season/episode did Aria train with Brienne of Tarth',
+            question: 'What season/episode did Aria train with Brienne of Tarth?',
                 a: 'Season 7: Episode 4',
                 b: 'Season 5: Episode 3', 
                 c: 'Season 6: Episode 9',
@@ -168,9 +168,11 @@ const showQuestion = () => {
     // this goes through the questions
     let q = questions[currentQuestion]
     // sets the question up on the page
-    question.innerHTML = "<p>"+ q.question +"</p>";
+    gameStart.style.display = 'block'
+    question.innerHTML = "<div>"+ q.question +"</div>";
+    question.classList.add('questions')
     // gives you choice A:
-    choiceA.innerText = q.a
+    choiceA.innerText =  q.a 
     // choice B:
     choiceB.innerText = q.b
     // choice C:
@@ -178,13 +180,17 @@ const showQuestion = () => {
     // this removes the regular game menu 
     section.style.display = 'none';
     option.style.display = 'none';
+    nuclear.style.display = 'block'
 
 }
 
 const showQuestion2 = () => {
     Array.from(document.querySelectorAll('.choice')).forEach(q => q.addEventListener('click', checkAnswer2))
+    gameStart.style.display = 'block'
     let q2 = questions2[currentQuestion]
+    
     question.innerHTML = "<p>"+ q2.question +"</p>";
+    question.classList.add('questions')
     choiceA.innerText = q2.a
     choiceB.innerText = q2.b
     choiceC.innerText = q2.c
@@ -196,8 +202,11 @@ const showQuestion3 = () =>
 {
 
     Array.from(document.querySelectorAll('.choice')).forEach(q => q.addEventListener('click', checkAnswer3))
+    gameStart.style.display = 'block'
     let q3 = questions3[currentQuestion]
+    
     question.innerHTML = "<p>"+ q3.question +"</p>";
+    question.classList.add('questions')
     choiceA.innerText = q3.a
     choiceB.innerText = q3.b
     choiceC.innerText = q3.c
@@ -208,9 +217,9 @@ const showQuestion3 = () =>
 
 // Check answer function:
 function checkAnswer(ev)  {
-let q = questions[currentQuestion]
+   
+    let q = questions[currentQuestion]
     if(q.correctAnswer == ev.target.id.toLowerCase() ){
-    console.log('correct')
     modal.style.display = 'block'
     if(currentQuestion < finalQuestion){
     currentQuestion++
@@ -219,57 +228,65 @@ let q = questions[currentQuestion]
     else 
     {
     noModal.style.display = 'block'
-     console.log('incorrect')   
     }
     
     } 
 
 
 function checkAnswer2(ev) {
+   
     let q2 = questions2[currentQuestion]
     if(q2.correctAnswer == ev.target.id.toLowerCase()){
-        console.log('correct')
-    if(currentQuestion < finalQuestion){
+        modal.style.display = 'block'
+        if(currentQuestion < finalQuestion){
         currentQuestion++
         showQuestion2()
     }
     }
     else {
-        console.log('incorrect')
+        noModal.style.display = 'block'
+        
     }
+    
 }
 
 function checkAnswer3(ev) {
     let q3 = questions3[currentQuestion]
     console.log(q3, ev.target.id.toLowerCase())
     if(q3.correctAnswer == ev.target.id.toLowerCase()){
-        console.log('second set')
-    if(currentQuestion < finalQuestion){
+        modal.style.display = 'block'
+        if(currentQuestion < finalQuestion){
         currentQuestion++
         showQuestion2()
         }
     }
     else {
-        
-        console.log('incorrect')
+        noModal.style.display = 'block'
         }
 }
 // new functionality
 function showOptions(ev){
     ev.preventDefault()
     option.style.display = 'block'
-    section.style.display = 'none';
-    
+    section.style.display = 'none'
+    nuclear.style.display = 'flex'
 }
 
 const showRules = () => {
-    rules.style.display = 'block';
+    rules.style.display = 'flex';
     rules.style.textAlign = "center"
     option.style.display = 'none';
-    section.style.display = 'none'
+    section.style.display = 'none';
+    nuclear.style.display = 'flex'
 }
-const closeModal = () => modal.style.display = 'none';
-close.addEventListener('click', closeModal)
+
+const closeModal = () => {
+    modal.style.display = 'none'
+    noModal.style.display = 'none'
+};
+
+
+close.forEach(close => close.addEventListener('click', closeModal))
 playBtn.addEventListener('click', showOptions)
 dragonBtn.addEventListener('click', showQuestion)
 houseBtn.addEventListener('click', showQuestion2)
